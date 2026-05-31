8377bbd6eb3624cf6e70d5c1230f3e019f6de467
kagome333 <kagome@dream.com>
2026-05-31 19:53:12 +0900

    Update SplashActivity.kt

--- app/src/main/java/com/example/secretimages/SplashActivity.kt
+++ app/src/main/java/com/example/secretimages/SplashActivity.kt
@@ -7,12 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent = if (PasswordManager.isPasswordSet(this)) {
-            Intent(this, UnlockActivity::class.java)
-        } else {
-            Intent(this, SetupPasswordActivity::class.java)
-        }
-        startActivity(intent)
+        startActivity(Intent(this, CalculatorActivity::class.java))
         finish()
     }
 }
